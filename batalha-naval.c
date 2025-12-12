b1eb3aa0cb18df7679cafe42496aa4274972edd3
Pedro Henrique Candido Nascimento <91481238+PH-CN@users.noreply.github.com>
2025-12-12 22:27:46 +0000

    desafio novato

--- batalha-naval.c
+++ batalha-naval.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
 
+
+main() {
+
+    int tabuleiro[10][10];
+    int tamtabuleiro = 10, tamnavio = 3;
+    int horizontalLinha = 3, horizontalColuna = 0; // índices da casa inicial do navio
+    int verticalLinha = 7, verticalColuna = 7; // índices da casa inicial do navio
+
+    // Preenche o tabuleiro inteiro com 0s
+    for(int i = 0; i < 10; i++) {
+        for(int n = 0; n < 10; n++) {
+            tabuleiro[i][n] = 0;
+        }
+    }
+
+    if(horizontalColuna + tamnavio > tamtabuleiro) {
+        printf("O navio horizontal está fora do tabuleiro!\n");
+        return 1;
+    }
+
+    if(verticalLinha + tamnavio > tamtabuleiro) {
+        printf("O navio vertical está fora do tabuleiro!\n");
+        return 1;
+    }
+
+    // Posicionando os navios no tabuleiro
+    for (int i = 0; i < tamnavio; i++) {
+        tabuleiro[horizontalLinha][horizontalColuna + i] = 3;
+        if(tabuleiro[verticalLinha + i][verticalColuna] != 0) {
+            printf("Sobreposição detectada!\n");
+            return 1;
+        }      // navio horizontal
+        tabuleiro[verticalLinha + i][verticalColuna] = 3;      // navio vertical
+    }
+
+    // Imprime o tabuleiro
+    for(int i = 0; i < 10; i++) {
+        for(int n = 0; n < 10; n++) {
+            printf("%d ", tabuleiro[i][n]);
+        }
+    printf("\n");
+    }
+
+    return 0;
+}
